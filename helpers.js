973d4cd8934b0ad4ed9ae4d57bd7e2313bfddb00
Cadebend <61368974+Cadebend@users.noreply.github.com>
2022-02-11 23:19:13 +0800

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,7 +17,7 @@ export const sdk = new ThirdwebSDK(
     // Wallet private key. NEVER CHECK THE KEY IN. ALWAYS USE ENVIRONMENT VARIABLES.
     process.env.WALLET_PRIVATE_KEY,
     // We use Polygon Mumbai network
-    ethers.getDefaultProvider("https://rpc-mumbai.maticvigil.com")
+    ethers.getDefaultProvider("https://matic-testnet-archive-rpc.bwarelabs.com")
   ),
 );
 
